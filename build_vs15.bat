29d27799510cbd7323c91b7ae68fd7474307fd34
Egor Seredin <4819888+agmt@users.noreply.github.com>
2023-12-19 22:43:49 +0900

    Build: print success edited

--- build_vs15.bat
+++ build_vs15.bat
@@ -10,3 +10,5 @@ if "%2" == "" set CONFIGURATION=Release
 set BUILD_CMD=MSBuild.exe quickfix_vs15.sln /t:%TARGET% /p:Configuration=%CONFIGURATION%
 echo Build command: %BUILD_CMD%
 %BUILD_CMD%
+
+echo "Success!!!"
